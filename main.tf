e30eb43fd4903e00fb5b2b548dff1ba9c8f85d1b
Srikanth Karra <karra.srikanth@gmail.com>
2021-11-24 18:17:50 +0530

    Initial TF code

---
+++ main.tf
@@ -0,0 +1,101 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet["name"]
+  address_space       = [var.vnet["address_space"]]
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet["name"]
+  address_prefixes     = [var.subnet["address_range"]]
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.subnet["name"]}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowSshInBound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHttpInBound"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet-nsg" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.vm_name}-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.vm_name}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip[count.index].id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.vm_size
+  admin_username      = var.username
+  admin_password      = var.password
+  network_interface_ids = [
+    azurerm_network_interface.nic[count.index].id,
+  ]
+
+  os_disk {
+    caching              = var.os_disk_caching
+    storage_account_type = var.os_disk_storage_account_type
+  }
+
+  source_image_reference {
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = "latest"
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,74 @@
+variable "rg" {
+  type    = string
+  default = "02-intermediate-rg"
+}
+
+variable "location" {
+  type    = string
+  default = "westus2"
+}
+
+variable "vnet" {
+  type = map(string)
+  default = {
+    "name"          = "intermediate-vnet"
+    "address_space" = "10.1.0.0/16"
+  }
+}
+
+variable "subnet" {
+  type = map(string)
+  default = {
+    "name"          = "subnet1"
+    "address_range" = "10.1.0.0/24"
+  }
+}
+
+variable "vm_name" {
+  type    = string
+  default = "intermediate-vm"
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_B2ms"
+}
+
+variable "os_disk_caching" {
+  type    = string
+  default = "ReadWrite"
+}
+
+variable "os_disk_storage_account_type" {
+  type    = string
+  default = "Standard_LRS"
+}
+
+variable "os_disk_size" {
+  default = 30
+}
+
+variable "publisher" {
+  type    = string
+  default = "Canonical"
+}
+
+variable "offer" {
+  type    = string
+  default = "UbuntuServer"
+}
+
+variable "sku" {
+  type    = string
+  default = "18.04-LTS"
+}
+
+variable "username" {
+  type    = string
+  default = "skarra"
+}
+
+variable "password" {
+  type    = string
+  default = "Azure@12345$"
+}
